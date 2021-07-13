da837d053a69eb0cb2741c063ddea3ba3f08fffb
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-13 12:57:49 -0400

    commit tree 3146834d3fdeaa40efa0335f6c15b95169e18220

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Smooth as chocolate', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 10, 'M', 'Black', 'Always angry', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'F', 'Rainbow', 'Gentle', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'M', 'Brown', 'Alpha', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 2, 'F', 'White', 'Mellow', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 12, 'M', 'Brown', 'Grumpy', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 9, 'F', 'Black', 'Protective', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 6, 'M', 'Brown', 'Dangerous', false);
